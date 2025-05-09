9de6ed7e4e3742711282eca9dc2ca133463f3994
SamuelPerez21 <samuel.perez2101@alumnos.ubiobio.cl>
2025-05-08 21:27:14 -0400

    Add ProductList component and integrate into dashboard
    - Created ProductListComponent with product listing functionality.
    - Updated dashboard to include navigation to the product list.
    - Enhanced dashboard actions to use router links for navigation.
    - Added CSS styles for the product list table.
    - Included unit tests for ProductListComponent.

--- frontend/ms-inventario/src/app/app.routes.ts
+++ frontend/ms-inventario/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
-
+import { ProductListComponent } from './components/product-list/product-list.component'; 
 
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent }, // Ruta raíz
+  { path: 'listar-productos', component: ProductListComponent}, // Ruta para listar productos
 ];
 
 @NgModule({
