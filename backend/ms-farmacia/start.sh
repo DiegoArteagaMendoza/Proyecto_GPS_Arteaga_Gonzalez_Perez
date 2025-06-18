0986b955a65a07e245397c6e5dfc3b69b3095210
DiegoArteagaMendoza <daarteagamendoza@gmail.com>
2025-06-17 22:56:45 -0400

    actualizacion de puerto

--- backend/ms-farmacia/start.sh
+++ backend/ms-farmacia/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Usamos el valor de PORT, o 8000 como fallback
-PORT=${PORT:-8000}
+PORT=${PORT:-8080}
 
 echo "Iniciando Gunicorn en puerto $PORT"
 exec gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
