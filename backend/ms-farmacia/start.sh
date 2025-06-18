3d3da2e7fb7586be35f96553bbebe76a306285ce
DiegoArteagaMendoza <daarteagamendoza@gmail.com>
2025-06-17 23:27:16 -0400

    migraciones

--- backend/ms-farmacia/start.sh
+++ backend/ms-farmacia/start.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# Usamos el valor de PORT, o 8000 como fallback
 PORT=${PORT:-8080}
+
+echo "Creando migraciones..."
+python manage.py makemigrations
+
+echo "Aplicando migraciones..."
+python manage.py migrate --noinput
 
 echo "Iniciando Gunicorn en puerto $PORT"
 exec gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
