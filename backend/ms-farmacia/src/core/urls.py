505269e49c48411dd012a6a7153cb4df1ed7b478
DiegoArteagaMendoza <daarteagamendoza@gmail.com>
2025-06-21 22:43:28 -0400

    configuracion para metricas de ms-farmacia

--- backend/ms-farmacia/src/core/urls.py
+++ backend/ms-farmacia/src/core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
-from farmacia.views import listar_farmacias, crear_farmacia
+from farmacia.views import listar_farmacias, crear_farmacia, metrics_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('metrics/', include('django_prometheus.urls')),
+    path('metrics/', metrics_view, name="metricas"),
     path("farmacias/", listar_farmacias, name="listar-farmacias"),
-    path("farmacias/registar/", crear_farmacia, name="registrar-farmacia"),
-    path("ruta/prueba/farmacia/", listar_farmacias, name="listar-farmacias"),
+    path("farmacias/registrar/", crear_farmacia, name="registrar-farmacia")
 ]
