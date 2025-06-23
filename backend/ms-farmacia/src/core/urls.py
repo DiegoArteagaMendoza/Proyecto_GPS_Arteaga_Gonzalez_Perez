37382370787d04308fed5d51284e8d8c432e6dde
DiegoArteagaMendoza <daarteagamendoza@gmail.com>
2025-06-23 08:44:37 -0400

    prueba

--- backend/ms-farmacia/src/core/urls.py
+++ backend/ms-farmacia/src/core/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('metrics/', metrics_view, name="metricas"),
     path("farmacias/", listar_farmacias, name="listar-farmacias"),
     path("farmacias/registrar/", crear_farmacia, name="registrar-farmacia"),
-    path("RutaPrueba/", crear_farmacia, name="Ruta-de-prueba")
+    # path("RutaPrueba/", crear_farmacia, name="Ruta-de-prueba")
 ]
