4c7080f3c5885d6eaaa0c89a76949fc8c2be3b93
DiegoArteagaMendoza <daarteagamendoza@gmail.com>
2025-07-20 14:53:09 -0400

    despliegue de frontend

---
+++ frontendPruebasLocal/build.js
@@ -0,0 +1,38 @@
+// build.js
+const fs = require('fs-extra');
+const path = require('path');
+
+// Definir las secciones a desplegar
+const SECCIONES = [
+    'inicio',
+    'ms-farmacia',
+    'ms-inventario',
+    'ms-trabajador',
+    'ms-usuariocliente',
+    'ms-venta'
+];
+
+const DEST = path.join(__dirname, 'dist');
+
+// Limpiar carpeta dist
+fs.emptyDirSync(DEST);
+
+SECCIONES.forEach(seccion => {
+    const SOURCE_HTML = path.join(__dirname, seccion, 'src', 'html');
+    const SOURCE_CSS = path.join(__dirname, seccion, 'src', 'css');
+    const SOURCE_JS = path.join(__dirname, seccion, 'src', 'js');
+    const DEST_SECCION = path.join(DEST, seccion);
+
+    // Copiar HTML
+    fs.copySync(SOURCE_HTML, DEST_SECCION);
+
+    // Copiar CSS
+    const DEST_CSS = path.join(DEST_SECCION, 'css');
+    fs.copySync(SOURCE_CSS, DEST_CSS);
+
+    // Copiar JS
+    const DEST_JS = path.join(DEST_SECCION, 'js');
+    fs.copySync(SOURCE_JS, DEST_JS);
+});
+
+console.log('✅ Build completado. Todas las secciones están listas en /dist/');
